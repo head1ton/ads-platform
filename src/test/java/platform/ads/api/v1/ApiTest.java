1912c70897002313f5ad271cf18e68334b399914
head1ton <head1ton@gmail.com>
2023-09-07 00:39:19 +0900

    19. [기능] 테스트를 위한 데이터 초기화 로직 및 랜덤포트 적용

---
+++ src/test/java/platform/ads/api/v1/ApiTest.java
@@ -0,0 +1,30 @@
+package platform.ads.api.v1;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApiTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @BeforeEach
+    void setUp() {
+        if (RestAssured.UNDEFINED_PORT == RestAssured.port) {
+            RestAssured.port = port;
+            databaseCleaner.afterPropertiesSet();
+        }
+        RestAssured.port = port;
+        databaseCleaner.afterPropertiesSet();
+
+        databaseCleaner.execute();
+    }
+}
---
+++ src/test/java/platform/ads/api/v1/member/service/MemberAuthServiceTest.java
@@ -0,0 +1,11 @@
+package platform.ads.api.v1.member.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class MemberAuthServiceTest {
+
+}
